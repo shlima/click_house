c919ff15b229472c76f4c0efd88c884528fdd165
Sead Feng <seadfeng@icoud.com>
2022-06-30 22:54:10 +0800

    Fixed ParseJson

--- lib/click_house/middleware/parse_json.rb
+++ lib/click_house/middleware/parse_json.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 require 'faraday_middleware/response_middleware'
 module ClickHouse
-    module Middleware
-      class ParseJson < FaradayMiddleware::ResponseMiddleware
-  
-        Faraday::Response.register_middleware self => self
-  
-        def call(environment)
-            @app.call(environment).on_complete do |env|
-              process_response(env) if parse_response?(env)
-            end
-        end
+  module Middleware
+    class ParseJson < FaradayMiddleware::ResponseMiddleware
+      Faraday::Response.register_middleware self => self
 
-        define_parser do |body, parser_options|
-            ::JSON.parse(body,  parser_options || {}) if !body.strip.empty? && body =~ /^\{/ 
+      def call(environment)
+        @app.call(environment).on_complete do |env|
+          process_response(env) if parse_response?(env)
         end
+      end
+
+      define_parser do |body, parser_options|
+        ::JSON.parse(body,  parser_options || {}) if !body.strip.empty? && body =~ /^\{/
       end
     end
   end
+end
   
